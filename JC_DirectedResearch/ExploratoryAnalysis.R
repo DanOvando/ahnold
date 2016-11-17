5b9592464febb51a84f694ec57bb6a55a5c9676a
jcvdav <juanarlos.villader@gmail.com>
2016-11-17 13:56:32 -0800

    Added BVI results only for bottom surveys

--- JC_DirectedResearch/ExploratoryAnalysis.R
+++ JC_DirectedResearch/ExploratoryAnalysis.R
@@ -1,64 +1,64 @@
 
+# Load packages
 library(tidyverse)
+library(plotly)
+library(Rbvi)
 
+# Load the list of sites and their categories
 sites <- read.csv("./MLPA Data/Final_Site_Table_UCSB.csv", stringsAsFactors = F) %>%
   select(SITE_SIDE, RESERVE)
 
+# Load the master spreadsheet for species
 master_spp <- read.csv("./MLPA Data/master_spp_table.csv", stringsAsFactors = F) %>%
   group_by(classcode, genus, species) %>%
   summarize(N = n()) %>%
   select(classcode, genus, species, N) %>%
   ungroup()
 
+ #Identify duplicated spp
 duplicated_sp <- filter(master_spp, N>1)
 
+# Get rid of the N column
 master_spp <- select(master_spp, -N)
 
-UCSB_fish <- read.csv("./MLPA Data/UCSB_FISH raw thru 2013.csv", stringsAsFactors = F)
+# Load the transect database, and filter it to keep only Bottom surveys
+UCSB_fish <- read.csv("./MLPA Data/UCSB_FISH raw thru 2013.csv", stringsAsFactors = F) %>%
+  filter(level == "BOT")
 
-data1 <- UCSB_fish %>%
-  left_join(master_spp, by = "classcode") %>%
-  mutate(SITE_SIDE = paste(site, side, sep = "_")) %>%
-  left_join(sites, by = "SITE_SIDE") %>%
-  mutate(sciname = paste(genus, species)) %>%
-  filter(!sciname == " ") %>%
-  group_by(day, month, year, site, side, RESERVE, zone, level, transect) %>%
-  summarize(N = sum(count)) %>%
-  mutate(key = paste(day, month, year, site, side, RESERVE, zone, level, transect)) %>%
-  ungroup() %>%
-  select(key, N)
-
+# Calculate averages per survey (average across transects)
 data2 <- UCSB_fish %>%
   left_join(master_spp, by = "classcode") %>%
   mutate(SITE_SIDE = paste(site, side, sep = "_")) %>%
   left_join(sites, by = "SITE_SIDE") %>%
   mutate(sample = paste(day, month, year, site, side, RESERVE, zone, level, transect, sep = "-"),
-         key = paste(day, month, year, site, side, RESERVE, zone, level, transect),
          sciname = paste(genus, species)) %>%
   filter(!sciname == " ") %>%
-  group_by(day, month, year, site, side, RESERVE, zone, level, transect, sample, key, sciname) %>%
+  group_by(day, month, year, site, side, RESERVE, zone, level, transect, sample, sciname) %>%
   summarize(count = sum(count)) %>%
-  group_by(month, year, site, side, RESERVE, sample, key, sciname) %>%
-  summarize(count = mean(count)) %>%
-  left_join(data1, by = "key") %>%
-  mutate(Ni = count/N,
-         date = as.Date(paste(1, month, year, sep = "/"), format = "%d/%m/%Y")) %>%
-  ungroup()
+  group_by(day, month, year, site, side, RESERVE, zone, sciname) %>%
+  summarize(N = mean(count)) %>%
+  ungroup() %>%
+  mutate(key = paste(day, month, year, site, side, RESERVE, zone))
 
-ggplotly(ggplot(data2, aes(x = date, y = Ni, color = sciname)) +
-           geom_line() +
-           geom_point() +
-           theme(legend.position="none"))
-
+# Keep the identifier, abundance and scientific name
 data3 <- ungroup(data2) %>%
   select(sciname, key, N) %>%
   spread(key, N)
 
+# Format this data to match the bvi input format
 rows <- data3$sciname
 data3<-select(data3, -sciname)
 data3 <- as.data.frame(data3)
 rownames(data3) <- rows
 data3 <- as.data.frame(data3)
 data3[is.na(data3)] = 0
-JC <- bvi()
+
+# Calculate bvi
+BVI <- bvi(data3)
+
+# Keep only BVI score and %BVI scores
+
+BVI2 <- BVI[,c(1,2837:2838)]
+BVI2
+  
 
