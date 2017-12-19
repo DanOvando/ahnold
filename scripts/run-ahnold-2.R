2a0723cc91f849e41dbb1f4e06af7d4adf96ba2a
Tyler Clavelle <tclavelle@bren.ucsb.edu>
2017-12-18 18:49:14 -0800

    prepare for stan run

--- scripts/run-ahnold-2.R
+++ scripts/run-ahnold-2.R
@@ -23,6 +23,10 @@ library(TMB)
 library(FishLife)
 library(patchwork)
 library(tidyverse)
+if (("demons" %in% installed.packages()) == F){
+  devtools::install_github('danovando/demons')
+}
+
 demons::load_functions('functions')
 
 run_name <- 'Working'
@@ -33,7 +37,7 @@ run_description <-
   'Model selection process, testing STAN selection'
 
 if (dir.exists(run_dir) == F) {
-  dir.create(run_dir)
+  dir.create(run_dir, recursive = T)
 }
 
 write(run_description,
@@ -44,7 +48,7 @@ write(run_description,
 
 run_length_to_density <-  F
 
-run_vast <- F # run VAST, best to leave off for now
+run_vast <- T # run VAST, best to leave off for now
 
 num_knots <-  10
 
